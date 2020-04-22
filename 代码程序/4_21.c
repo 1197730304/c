8d420f92ef789c17be31f6f646a4734cd75dfad4
MikeLiu015751 <59679901+MikeLiu015751@users.noreply.github.com>
2020-04-22 22:20:25 +0800

    Add files via upload

--- 代码程序/4_21.c
+++ 代码程序/4_21.c
@@ -1,33 +1,71 @@
-bool isbalance(struct TreeNode* root)
+//平衡二叉树
+
+int geth(struct TreeNode* root)
 {
-	if (root == NULL)
-		return true;
-	int l = geth(root->left);
-	int r = geth(root->right);
-	int h = abs(l - r);
-	return h < 2&& isbalance(root->left) &&
-		isbalance(root->right);
+    if (root == NULL)
+        return 0;
+    int l = geth(root->left);
+    int r = geth(root->right);
+    return l > r ? l + 1 : r + 1;
+}
+bool isBalanced(struct TreeNode* root) {
+    if (root == NULL)
+        return true;
+    int l = geth(root->left);
+    int r = geth(root->right);
+    return (abs(l - r) < 2) && isBalanced(root->left) &&
+        isBalanced(root->right);
 }
 
 
-int numberK(struct TreeNode* root,int k)
+//二叉树的遍历oj
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct TreeNode
 {
-	if (root == NULL)
-		return 0;
-	if (k == 1)
-		return 1;
-	return numberK(root->left, k - 1) + numberK(root->right, k - 1);
+    struct TreeNode* left;
+    struct TreeNode* right;
+    char val;
+}TreeNode;
+
+//建树,先序遍历
+TreeNode* TreeNodeCreat(char* str, int* idx)
+{
+    if (str[*idx] != '#')
+    {
+        TreeNode* root = (TreeNode*)malloc(sizeof(TreeNode));
+        root->val = str[*idx];
+        (*idx)++;
+        root->left = TreeNodeCreat(str, idx);
+        (*idx)++;
+        root->right = TreeNodeCreat(str, idx);
+        return root;
+    }
+    else
+        return NULL;
+}
+//中序遍历
+void TreeNodemid(TreeNode* root)
+{
+    if (root)
+    {
+        TreeNodemid(root->left);
+        printf("%c ", root->val);
+        TreeNodemid(root->right);
+
+    }
+
 
 }
 
-BTNode* BinaryTreeFind(BTNode* root, BTDataType x)
+int main()
 {
-	if (root == NULL)
-		return NULL;
-	if (root->val == x)
-	{
-		return root;
-	}
-	BinaryTreeFind(root->left, x);
-	BinaryTreeFind(root->right, x);
+    char str[101] = { 0 };
+    int idx = 0;
+    scanf("%s", str);
+    TreeNode* root = TreeNodeCreat(str, &idx);
+    TreeNodemid(root);
+    return 0;
+
 }
